19e3cb328581f8af70ca9f079e466e9a0b8f6c85
Holger Pirk <holger@pirk.name>
2024-03-10 22:42:25 +0000

    add script to create new engine

---
+++ CreateNewEngine.cmake
@@ -0,0 +1,99 @@
+math(EXPR LAST_ARGC "${CMAKE_ARGC} - 1") 
+
+set(PROJECT_NAME "${CMAKE_ARGV${LAST_ARGC}}")
+message("generating new project: ${PROJECT_NAME}")
+
+file(WRITE ${PROJECT_NAME}/CMakeLists.txt
+"cmake_minimum_required(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})\n"
+"project(${PROJECT_NAME})\n"
+[=[
+
+############################## Custom build modes ###############################
+
+set(CMAKE_CXX_FLAGS_SANITIZE "-fno-omit-frame-pointer -fsanitize=address,signed-integer-overflow,null,alignment,bounds,function,return,vla-bound -O0 -g"
+  CACHE STRING
+  "Flags used by the C++ compiler during Sanitize builds."
+  FORCE)
+set(CMAKE_C_FLAGS_SANITIZE "-fno-omit-frame-pointer -fsanitize=address,signed-integer-overflow,null,alignment,bounds,function,return,vla-bound -O0 -g"
+  CACHE STRING
+  "Flags used by the C compiler during Sanitize builds."
+  FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_SANITIZE
+  ${CMAKE_EXE_LINKER_FLAGS_DEBUG} CACHE STRING
+  "Flags used for linking binaries during Sanitize builds."
+  FORCE)
+set(CMAKE_SHARED_LINKER_FLAGS_SANITIZE
+  ${CMAKE_SHARED_LINKER_FLAGS_DEBUG} CACHE STRING
+  "Flags used by the shared libraries linker during Sanitize builds."
+  FORCE)
+mark_as_advanced(
+  CMAKE_CXX_FLAGS_SANITIZE		  CMAKE_EXE_LINKER_FLAGS_SANITIZE
+  CMAKE_C_FLAGS_SANITIZE		  CMAKE_SHARED_LINKER_FLAGS_SANITIZE
+  )
+
+set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING
+  "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel Sanitize."
+  FORCE)
+
+############################### External Projects ###############################
+
+include(ExternalProject)
+
+set(BOSS_SOURCE_BRANCH main CACHE STRING "if boss core is built from source, which thread should be fetched")
+set(BOSS_SOURCE_REPOSITORY git@github.com:Bossdb/Core.git CACHE STRING "url for the boss repository")
+]=]
+"ExternalProject_Add(BOSS
+  GIT_REPOSITORY \${BOSS_SOURCE_REPOSITORY}
+  DOWNLOAD_DIR $ENV{HOME}/.cmake-downloads/\${CMAKE_PROJECT_NAME}
+  GIT_TAG \${BOSS_SOURCE_BRANCH}
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=\${${PROJECT_NAME}_BINARY_DIR}/deps $<IF:$<CONFIG:>,,-DCMAKE_BUILD_TYPE=\${CMAKE_BUILD_TYPE}> -DCMAKE_CXX_COMPILER=\${CMAKE_CXX_COMPILER}  -DCMAKE_C_COMPILER=\${CMAKE_C_COMPILER} -DCMAKE_CXX_FLAGS=\${CMAKE_CXX_FLAGS}
+  )
+
+
+#################################### Targets ####################################
+if(MSVC)
+  # not making any difference on Windows
+  # and helps Visual Studio to parse it correctly as a target
+  set(LibraryType SHARED)
+else()
+  set(LibraryType MODULE)
+endif(MSVC)
+add_library(${PROJECT_NAME} \${LibraryType} Source/${PROJECT_NAME}.cpp)
+
+set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD 23)
+if(MSVC)
+  target_compile_options(${PROJECT_NAME} PUBLIC \"/Zc:__cplusplus\")
+  target_compile_options(${PROJECT_NAME} PUBLIC \"/EHsc\")
+endif(MSVC)
+target_include_directories(${PROJECT_NAME} SYSTEM PUBLIC \${${PROJECT_NAME}_BINARY_DIR}/deps/include)
+add_dependencies(${PROJECT_NAME} BOSS)
+
+set_target_properties(${PROJECT_NAME} PROPERTIES INSTALL_RPATH_USE_LINK_PATH TRUE)
+install(TARGETS ${PROJECT_NAME} LIBRARY DESTINATION lib)
+"
+)
+
+file(WRITE ${PROJECT_NAME}/Source/${PROJECT_NAME}.cpp
+[=[
+#include <BOSS.hpp>
+#include <Expression.hpp>
+#include <ExpressionUtilities.hpp>
+#include <Utilities.hpp>
+
+using std::string_literals::operator""s;
+using boss::utilities::operator""_;
+using boss::ComplexExpression;
+using boss::Span;
+using boss::Symbol;
+
+using boss::Expression;
+
+static Expression evaluate(Expression &&e) {
+  return std::move(e);
+};
+
+extern "C" BOSSExpression* evaluate(BOSSExpression* e) {
+  return new BOSSExpression{.delegate = evaluate(std::move(e->delegate))};
+};
+]=]
+)
